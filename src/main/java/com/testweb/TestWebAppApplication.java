7a8243c83f6d863411061a87453513976b929ab1
LED1990 <30773740+LED1990@users.noreply.github.com>
2017-10-03 18:43:47 +0200

    1.0

---
+++ src/main/java/com/testweb/TestWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.testweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWebAppApplication.class, args);
+	}
+
+	//added some comment but it doesnt work
+}
